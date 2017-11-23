971662a88611ab52632a22fc3ac8657368f878d3
Jerrin Finney <jfinn8808@gmail.com>
2017-11-23 23:04:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+let logShout = "hello"
+If (function logShout(string)){
+  console.log("I can't hear you")
+}
